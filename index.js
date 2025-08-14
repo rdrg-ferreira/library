3ad506000cd9d37629e2692f3648dfa62ba2b5eb
Rodrigo Ferreira <rodrigo.slva.ferreira@gmail.com>
2025-08-14 18:20:34 +0100

    button options for a book

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const books = document.querySelectorAll(".book");
+
 let library = [];
 
 function Book(title, author) {
@@ -9,4 +11,29 @@ function Book(title, author) {
 function addNewBook(title, author) {
     const book = new Book(title, author);
     library.push(book)
+
+    displayLibrary();
 }
+
+function displayLibrary() {
+
+}
+
+// event listeners
+
+books.forEach(book => {
+    book.addEventListener("mouseover", () => {
+        const bookOptions = book.querySelector(".features");
+        bookOptions.classList.add("visible");
+        console.log("visible");
+    })
+});
+
+
+books.forEach(book => {
+    book.addEventListener("mouseout", () => {
+        const bookOptions = book.querySelector(".features");
+        bookOptions.classList.remove("visible");
+        console.log("not visible");
+    })
+});
